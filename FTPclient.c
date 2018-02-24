440b780aa35074e20e3782595c50285744bae72a
Helw150 <wbh230@nyu.edu>
2018-02-24 17:27:53 +0400

    Client to interfact with server and can handle local commands

--- FTPclient.c
+++ FTPclient.c
@@ -9,14 +9,15 @@
 #include <FTPclient.h>
 #include <helpers.h>
 
-#define PORT 8080
 
 int main(int argc, char const *argv[])
 {
+    char *ip_addr = argv[1];
+    struct CommandRegex commands = compileAllCommandChecks();
+    int PORT = atoi(argv[2]);
     struct sockaddr_in address;
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
     char buffer[1024] = {0};
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
@@ -30,7 +31,7 @@ int main(int argc, char const *argv[])
     serv_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET, ip_addr, &serv_addr.sin_addr)<=0)
 	{
 	    printf("\nInvalid address/ Address not supported \n");
 	    return -1;
@@ -41,11 +42,47 @@ int main(int argc, char const *argv[])
 	    printf("\nConnection Failed \n");
 	    return -1;
 	}
+
+    getpeername(sock, (struct sockaddr *)&address, sizeof(address));
+    char input[128];
     while(1){
-	send(sock , hello , strlen(hello) , 0 );
-	printf("Hello message sent\n");
-	valread = read( sock , buffer, 1024);
-	printf("%s\n",buffer );
+	memset(&buffer[0], 0, sizeof(buffer));
+	printf("ftp> ");
+	fgets(input, 128, stdin);
+	if(input[0] == '!'){
+	    // Remove the local spec
+	    char* local_input = stripStartingChars(1, input);
+	    localCommand(local_input, commands);
+	}
+	else if(checkRegex(commands.QUIT, input)){
+	    return 0;
+	} else {
+	    send(sock , input, strlen(input) , 0 );
+	    valread = read( sock , buffer, 1024);
+	    printf("%s",buffer );
+	}
     }
     return 0;
+}
+
+void localCommand(char* input, struct CommandRegex commands){
+    char cwd[1024];
+    getcwd(cwd, strlen(cwd));
+    if(checkRegex(commands.LS, input)){
+	printf("%s", lsCommand(cwd));
+    } else if (checkRegex(commands.PWD, input)) {
+	printf("%s", cwd);
+    } else if (checkRegex(commands.CD, input)) {
+	char* dir = stripStartingChars(commands.cd_len, input);
+	int ret = chdir(dir);
+	if(ret == 0){ // If the directory is valid
+	    char *tmp = (char *) malloc(sizeof(char) * 1024);
+	    getcwd(tmp, 1024);
+	    // Return the pwd command so they know where they changed to
+	    printf(pwdCommand(tmp));
+	} else {
+	    printf("Invalid target directory!\n");
+	}
+    }
+    return;
 }
